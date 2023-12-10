9d63c2dd8be90790bd33782463a665ca692bd32e
Yugal Rana Magar <72245757+Yugalranamagar@users.noreply.github.com>
2023-12-11 00:55:49 +0545

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 20%;
+  height:50%;
+  float:left;
+  padding:0 15% 0 15%;
+}
+.card:hover{
+  box-shadow: 0 10px 20px 0 rgba(0,0,0,0.2);
+}
+p{
+  font-family:bold;
+  font-size:1vw;
+}
+.container {
+  padding: 2px 16px;
+}
+.h4{
+  font-size:2vw;
+}
+</style>
+</head>
+<body>
+
+<h2>Contact us</h2>
+
+<div class="card">
+  <img src="yugal.jpg" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Yugal Rana</b></h4> 
+    <p>Founder</p> 
+    <p> MINI-QUIZ</p>
+    <a href="https://www.facebook.com/yugalranamgr"><img src="fb.png" alt="Avatar" style="width:15%"></a>
+    <a href="https://myaccount.google.com/profile?pli=1""><img src="gmail.png" alt="Avatar" style="width:15%"></a>
+    <a href="https://www.whatsapp.com/"><img src="wa.png" alt="Avatar" style="width:15%"></a>
+    <a href="https://www.instagram.com/yugal_rana_magar"><img src="ig.png" alt="Avatar" style="width:15%"></a>
+  </div>
+</div>
+
+<div class="card">
+  <img src="thaman.jpg" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Thaman Rana</b></h4> 
+    <p>Founder</p> 
+    <p> MINI-QUIZ</p>
+    <a href="https://www.facebook.com/thaman.magar.904750"><img src="fb.png" alt="Avatar" style="width:15%"></a>
+    <a href="https://myaccount.google.com/profile?pli=1"><img src="gmail.png" alt="Avatar" style="width:15%"></a>
+    <a href="https://www.whatsapp.com/"><img src="wa.png" alt="Avatar" style="width:15%"></a>
+    <a href="https://www.instagram.com/thamanrana47"><img src="ig.png" alt="Avatar" style="width:15%"></a>
+    
+  </div>
+</div>
+</body>
+</html> 
