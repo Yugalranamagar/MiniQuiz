3d36e589b034429fe52d4e2e173dbc09c41ce909
Yugal Rana Magar <72245757+Yugalranamagar@users.noreply.github.com>
2023-12-11 00:57:34 +0545

    Add files via upload

---
+++ admin/login.php
@@ -0,0 +1,15 @@
+<?php
+$localhost="localhost";
+$username="root";
+$password="";
+$dbname="demo";
+$conn=mysqli_connect($localhost,$username,$password,$dbname);
+if($conn)
+{
+
+}
+else
+{
+
+}
+?>
